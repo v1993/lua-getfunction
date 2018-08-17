7932fc81035929cb6accb8f2b5c9f82417954632
v1993 <v19930312@gmail.com>
2018-08-18 01:41:18 +0300

    Return nothing instead of error if too deep

--- getfunction.c
+++ getfunction.c
@@ -5,7 +5,7 @@ static int l_getfunction(lua_State *L) {
 	lua_Debug ar;
 	int level = luaL_checkinteger(L, 1);
 	if (level < 0) luaL_error(L, "invalid stack level");
-	if (lua_getstack(L, level, &ar) != 1) luaL_error(L, "failed to get stack frame");
+	if (lua_getstack(L, level, &ar) != 1) return 0; // слишком глубоко
 	if (lua_getinfo(L, "f", &ar) == 0) luaL_error(L, "failed to get function");
 	return 1;
 }
