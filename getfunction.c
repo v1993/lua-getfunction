85c3fff0215ecf385011b5649eeed72f064eba1b
v1993 <v19930312@gmail.com>
2018-08-18 01:31:01 +0300

    Inital commit

---
+++ getfunction.c
@@ -0,0 +1,16 @@
+#include "lua.h"
+#include "lauxlib.h"
+
+static int l_getfunction(lua_State *L) {
+	lua_Debug ar;
+	int level = luaL_checkinteger(L, 1);
+	if (level < 0) luaL_error(L, "invalid stack level");
+	if (lua_getstack(L, level, &ar) != 1) luaL_error(L, "failed to get stack frame");
+	if (lua_getinfo(L, "f", &ar) == 0) luaL_error(L, "failed to get function");
+	return 1;
+}
+
+int luaopen_getfunction(lua_State *L) {
+	lua_pushcfunction(L, l_getfunction); // require вернёт нашу функцию
+	return 1;
+}
